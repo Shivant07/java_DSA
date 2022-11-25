8f2027ffee74017a3cf5defd270cfc10547ea44f
Shivant kumar <115063978+Shivant07@users.noreply.github.com>
2022-11-25 12:51:03 +0530

    Add files via upload

---
+++ Branch.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Branch {
+    static void fun(int num){
+        if(num<=0){
+            return;
+        }
+        System.out.print(num + " ");
+        fun(num-1);
+        fun(num-2);
+    }
+
+    public static void main(String[] args) {
+        fun(4);
+        
+
+    }
+    
+}
